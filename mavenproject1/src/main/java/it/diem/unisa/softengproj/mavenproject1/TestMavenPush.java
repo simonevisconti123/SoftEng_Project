550c2941639698f56144685ba9fbed2aabbf9438
Anthony <>
2024-11-22 18:32:45 +0100

    Test commit del progetto di test

---
+++ mavenproject1/src/main/java/it/diem/unisa/softengproj/mavenproject1/TestMavenPush.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.diem.unisa.softengproj.mavenproject1;
+
+/**
+ *
+ * @author Anthony
+ */
+public class TestMavenPush {
+    
+    
+public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
