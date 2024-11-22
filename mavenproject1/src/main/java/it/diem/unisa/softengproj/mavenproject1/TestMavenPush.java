21be10366743466b451e435c6c173170769f581b
Simone <>
2024-11-22 18:39:49 +0100

    Aggiunta di stampe di test

--- mavenproject1/src/main/java/it/diem/unisa/softengproj/mavenproject1/TestMavenPush.java
+++ mavenproject1/src/main/java/it/diem/unisa/softengproj/mavenproject1/TestMavenPush.java
@@ -14,6 +14,6 @@ public class TestMavenPush {
     
 public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Stampa di prova"); // stampa di prova
     }
 }
-
