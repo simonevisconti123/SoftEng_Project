d58f00a8379e3dbffd6581abc2af99c902bd29a7
Simone <>
2024-12-10 12:59:23 +0100

    Creazione progetto gluon

---
+++ Rubrica_Telefonica_Gluon/src/main/java/gruppo06/rubrica_telefonica_gluon/SecondaryController.java
@@ -0,0 +1,12 @@
+package gruppo06.rubrica_telefonica_gluon;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
---
+++ Rubrica_Telefonica_Gluon/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module gruppo06.rubrica_telefonica_gluon {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens gruppo06.rubrica_telefonica_gluon to javafx.fxml;
+    exports gruppo06.rubrica_telefonica_gluon;
+}
+*/
